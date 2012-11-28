859d1deeea4065b056c1031040f77b24870e6891
viblo <vb-github@viblo.se>
2012-11-28 15:34:24 +0100

    Added project url to assembly description

--- PaysonIntegration/Properties/AssemblyInfo.cs
+++ PaysonIntegration/Properties/AssemblyInfo.cs
@@ -6,13 +6,14 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PaysonIntegration")]
-[assembly: AssemblyDescription("Payson API library to help integration against the Payson API")]
+[assembly: AssemblyDescription("Payson API library to help integration against the Payson API. Project located at: https://github.com/PaysonAB")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Payson")]
 [assembly: AssemblyProduct("PaysonIntegration")]
 [assembly: AssemblyCopyright("Copyright © Payson 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
