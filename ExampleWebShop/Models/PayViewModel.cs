5acde063d6663c77e55017a1e1ff46dd161075e6
viblo <vb-github@viblo.se>
2013-02-21 09:22:08 +0100

    Changed datatype of Quantity from int to decimal.

--- ExampleWebShop/Models/PayViewModel.cs
+++ ExampleWebShop/Models/PayViewModel.cs
@@ -6,7 +6,7 @@ namespace ExampleWebShop.Models
     {
         public string Description { get; set; }
         public string Sku { get; set; }
-        public int Quantity { get; set; }
+        public decimal Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal TaxPercentage { get; set; }
     }
