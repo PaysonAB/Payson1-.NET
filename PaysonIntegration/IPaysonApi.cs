3b23b5e93b4069a017d183e516cdb89ac4fe98e6
Daniel Hansen <daniel.hansen@payson.se>
2013-05-13 16:35:59 +0200

    Created a interface and implemented this to aid in unit testing.

---
+++ PaysonIntegration/IPaysonApi.cs
@@ -0,0 +1,16 @@
+﻿using PaysonIntegration.Data;
+using PaysonIntegration.Response;
+
+namespace PaysonIntegration
+{
+    public interface IPaysonApi
+    {
+        int Timeout { get; set; }
+        bool IsTestMode { get; set; }
+        string GetForwardPayUrl(string token);
+        PayResponse MakePayRequest(PayData data);
+        PaymentUpdateResponse MakePaymentUpdateRequest(PaymentUpdateData data);
+        PaymentDetailsResponse MakePaymentDetailsRequest(PaymentDetailsData data);
+        ValidateResponse MakeValidateIpnContentRequest(string content);
+    }
+}
